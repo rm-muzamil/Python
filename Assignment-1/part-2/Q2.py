11a09aac54bf7bbc7f9d8bb1db939c157219c864
rm muzamil <ramuzamil44@gmail.com>
2024-11-08 23:20:47 +0500

    secound part in process

--- Assignment-1/part-2/Q2.py
+++ Assignment-1/part-2/Q2.py
@@ -1,1 +1,5 @@
 # Use a while loop to print even numbers from 1 to 50.
+num = 2
+while num <= 50:
+    print(num)
+    num += 2
---
+++ Assignment-1/part-2/Q3.py
@@ -0,0 +1,5 @@
+# write a program that calculate the sum of all numbers between 1 and 100
+sum = 0
+for x in range(1, 101):
+    sum += x
+print(sum)
